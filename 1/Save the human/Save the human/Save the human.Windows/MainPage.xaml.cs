aef0f60082720feec7bf77c24564f8a249a53c48
maxim58 <maxim.bauman@yandex.ru>
2017-02-09 12:19:44 +0400

    Save the humans

---
+++ 1/Save the human/Save the human/Save the human.Windows/MainPage.xaml.cs
@@ -0,0 +1,235 @@
+﻿using Save_the_human.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
+
+// Документацию по шаблону элемента "Основная страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Save_the_human
+{
+    /// <summary>
+    /// Основная страница, которая обеспечивает характеристики, являющимися общими для большинства приложений.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Random random = new Random();
+        DispatcherTimer enemyTimer = new DispatcherTimer();
+        DispatcherTimer targetTimer = new DispatcherTimer();
+        bool humanCaptured = false;
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// Эту настройку можно изменить на модель строго типизированных представлений.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper используется на каждой странице для облегчения навигации и 
+        /// управление жизненным циклом процесса
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            enemyTimer.Tick += enemyTimer_Tick;
+            enemyTimer.Interval = TimeSpan.FromSeconds(2);
+
+            targetTimer.Tick += targetTimer_Tick;
+            targetTimer.Interval = TimeSpan.FromSeconds(1);
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        void targetTimer_Tick(object sender, object e)
+        {
+            progressBar.Value += 1;
+            if (progressBar.Value >= progressBar.Maximum)
+                EndTheGame();
+        }
+
+        private void EndTheGame()
+        {
+            enemyTimer.Stop();
+            targetTimer.Stop();
+            humanCaptured = false;
+            startButton.Visibility = Visibility.Visible;
+            playArea.Children.Add(gameOverText);
+        }
+
+        void enemyTimer_Tick(object sender, object e)
+        {
+            AddEnemy();
+        }
+
+        /// <summary>
+        /// Заполняет страницу содержимым, передаваемым в процессе навигации. Любое сохраненное состояние также является
+        /// при повторном создании страницы из предыдущего сеанса.
+        /// </summary>
+        /// <param name="sender">
+        /// Источник события; как правило, <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Данные события, предоставляющие параметр навигации, который передается
+        /// <see cref="Frame.Navigate(Type, Object)"/> при первоначальном запросе этой страницы, и
+        /// словарь состояния, сохраненного этой страницей в ходе предыдущего
+        /// сеансом. Состояние будет равно значению NULL при первом посещении страницы.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Сохраняет состояние, связанное с данной страницей, в случае приостановки приложения или
+        /// удаления страницы из кэша навигации.  Значения должны соответствовать требованиям сериализации
+        /// <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">Источник события; как правило, <see cref="NavigationHelper"/></param>
+        /// <param name="e">Данные события, которые предоставляют пустой словарь для заполнения
+        /// сериализуемым состоянием.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region Регистрация NavigationHelper
+
+        /// Методы, предоставленные в этом разделе, используются исключительно для того, чтобы
+        /// NavigationHelper для отклика на методы навигации страницы.
+        /// 
+        /// Логика страницы должна быть размещена в обработчиках событий для 
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// и <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// Параметр навигации доступен в методе LoadState 
+        /// в дополнение к состоянию страницы, сохраненному в ходе предыдущего сеанса.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            human.IsHitTestVisible = true;
+            humanCaptured = false;
+            progressBar.Value = 0;
+            startButton.Visibility = Visibility.Collapsed;
+            playArea.Children.Clear();
+            playArea.Children.Add(target);
+            playArea.Children.Add(human);
+            enemyTimer.Start();
+            targetTimer.Start();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
+            playArea.Children.Add(enemy);
+
+            enemy.PointerEntered += enemy_PointerEntered;
+        }
+
+        void enemy_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured) EndTheGame();
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        private void human_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (enemyTimer.IsEnabled)
+            {
+                humanCaptured = true;
+                human.IsHitTestVisible = false;
+            }
+        }
+
+        private void target_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (targetTimer.IsEnabled && humanCaptured)
+            {
+                progressBar.Value = 0;
+                Canvas.SetLeft(target, random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(target, random.Next(100, (int)playArea.ActualHeight - 100));
+                Canvas.SetLeft(human, random.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(human, random.Next(100, (int)playArea.ActualHeight - 100));
+                humanCaptured = false;
+                human.IsHitTestVisible = true;
+            }
+        }
+
+        private void playArea_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+            {
+                Point pointerPosition = e.GetCurrentPoint(null).Position;
+                Point relativePosition = grid.TransformToVisual(playArea).TransformPoint(pointerPosition);
+                if ((Math.Abs(relativePosition.X-Canvas.GetLeft(human))> human.ActualWidth*3)||(Math.Abs(relativePosition.Y - Canvas.GetTop(human))>human.ActualHeight*3))
+                {
+                    humanCaptured = false;
+                    human.IsHitTestVisible = true;
+                }
+                else
+                {
+                    Canvas.SetLeft(human, relativePosition.X - human.ActualWidth / 2);
+                    Canvas.SetTop(human, relativePosition.Y - human.ActualHeight / 2);
+                }
+            }
+        }
+
+        private void playArea_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
+        }
+    }
+}
